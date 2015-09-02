7e466af6668ae9eaf3ca9d9c64a42ae24444bf67
Gabriel A. Devenyi <gdevenyi@gmail.com>
2015-09-02 11:18:02 -0400

    Many changes, post server transition

--- initial.sh
+++ initial.sh
@@ -1,1 +1,2 @@
-ansible-playbook -K -k -vv -i inventory initial-setup.yml
+ansible-playbook -K -k -vv -i inventory initial-setup.yml --limit $1
+#ansible-playbook -vv -i inventory site.yml --limit $1

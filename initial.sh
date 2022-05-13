b10c78ff1f8cc3ab8019d903adb570875df50a02
Gabriel A. Devenyi <gdevenyi@gmail.com>
2022-05-13 14:37:17 -0400

    Update for 22.04 Kubuntu

--- initial.sh
+++ initial.sh
@@ -1,2 +1,3 @@
 ansible-playbook -K -k -vv -i inventory initial-setup.yml --limit $1
-#ansible-playbook -vv -i inventory site.yml --limit $1
+ansible-playbook -vv -i inventory site.yml --limit $1
+ansible-playbook -vv -i inventory update.yml --limit $1

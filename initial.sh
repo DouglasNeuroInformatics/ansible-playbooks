08bbb9d9f31a435d0043d27f477b515d793025d5
Gabriel A. Devenyi <gdevenyi@gmail.com>
2022-05-31 14:47:58 -0400

    Update to 22.04

--- initial.sh
+++ initial.sh
@@ -1,3 +1,4 @@
 ansible-playbook -K -k -vv -i inventory initial-setup.yml --limit $1
 ansible-playbook -vv -i inventory site.yml --limit $1
 ansible-playbook -vv -i inventory update.yml --limit $1
+ansible -b -m reboot all --limit ${1}

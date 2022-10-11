8a710939f53aa3ab32e2f099a2ff20352ed826ab
Gabriel A. Devenyi <gdevenyi@gmail.com>
2022-10-11 16:51:57 -0400

    Complete bios and motherboard auditing

--- bin/bios_mobo_versions.sh
+++ bin/bios_mobo_versions.sh
@@ -1,1 +1,1 @@
-ansible -v -b -m shell -a 'lshw -json' all
+ansible -v -b -m shell -a 'echo $(hostname),$(lshw -json | jq ".children[0].product"),$(lshw -json | jq ".children[0].children[0].version")' all | grep -E '(cic|dnp)ws[0-9]+,'

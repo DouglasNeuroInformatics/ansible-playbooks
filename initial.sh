5fee0ddbd684fe5ce1ee67777e1608ebbc9e1332
Gabriel A. Devenyi <gdevenyi@gmail.com>
2022-10-11 16:14:25 -0400

    Move and update sh files

--- initial.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-ansible-playbook -K -k -v -i inventory initial-setup.yml --limit $1
-ansible-playbook -v -i inventory site.yml --limit $1
-ansible-playbook -v -i inventory update.yml --limit $1
-ansible -b -m reboot all --limit ${1}
--- reboot-if-empty.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# [ $(who | grep -v -c localadmin) -eq 0 ] 
-
-
-ansible -v -b -m shell -a '[ $(who | grep -v -c localadmin) -eq 0 ] && reboot' workstations

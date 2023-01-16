0985fa89adf98d85b06e2ad097f8562933c8ddae
Gabriel A. Devenyi <gdevenyi@gmail.com>
2023-01-16 15:30:33 -0500

    Add some more helper standalone calls

--- bin/reboot-if-empty.sh
+++ bin/reboot-if-empty.sh
@@ -3,4 +3,4 @@
 # [ $(who | grep -v -c localadmin) -eq 0 ] 
 
 
-ansible -v -b -m shell -a '[ $(who | grep -v -c localadmin) -eq 0 ] && reboot' workstations
+ansible -v -b -m shell -a '[ $(who | grep -v -c localadmin) -eq 0 ] && apt update && apt full-upgrade -y && apt-get purge .*nvidia.* -y && apt-get --purge autoremove -y && ubuntu-drivers install && reboot' workstations

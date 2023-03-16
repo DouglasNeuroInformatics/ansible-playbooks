ece7be19e6bf7d79d25b5ae5a2023da5563ffa6f
Gabriel A. Devenyi <gdevenyi@gmail.com>
2023-03-16 12:08:32 -0400

    Enable local redirect of XDG_CACHE variable

---
+++ files/xdg.cache.sh
@@ -0,0 +1,16 @@
+# could be more paranoid, and not accept any previously defined XDG_CACHE_HOME
+if [ -z "${XDG_CACHE_HOME}" ] ; then
+  XDG_CACHE_HOME="/var/tmp/xdgcache-${USER}"
+  export XDG_CACHE_HOME
+fi
+
+if [ -d "${XDG_CACHE_HOME}" ]; then
+  # verify existing dir is suitable
+  if ! `test -G "${XDG_CACHE_HOME}" -a -w "${XDG_CACHE_HOME}"` ; then
+    # else, make a new/secure one with mktemp
+    XDG_CACHE_HOME="$(mktemp -d ${XDG_CACHE_HOME}-XXXXXX)"
+    export XDG_CACHE_HOME
+  fi
+else
+  mkdir -p "${XDG_CACHE_HOME}"
+fi

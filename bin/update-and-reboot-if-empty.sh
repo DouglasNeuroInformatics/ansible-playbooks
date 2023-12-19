ff54c0dcc1d87a45450d15630ab49e4675f15677
Gabriel A. Devenyi <gdevenyi@gmail.com>
2023-12-19 12:28:52 -0500

    Remove old script

--- bin/update-and-reboot-if-empty.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# [ $(who | grep -v -c localadmin) -eq 0 ] 
-
-
-ansible -v -b -m shell -a '[ $(who | grep -v -c localadmin) -eq 0 ] && apt update && apt full-upgrade -y -o Dpkg::Options::="--force-confdef" && apt-get purge .*nvidia.* -y && apt-get --purge autoremove -y && ubuntu-drivers install && reboot' workstations

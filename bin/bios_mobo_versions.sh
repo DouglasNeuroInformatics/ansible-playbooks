35976f54e5558741e1bcc7a0224a3bb3cfb1c0ad
Gabriel A. Devenyi <gdevenyi@gmail.com>
2024-10-02 14:02:23 -0400

    Get serial number and expand machines which are queried

--- bin/bios_mobo_versions.sh
+++ bin/bios_mobo_versions.sh
@@ -1,1 +1,1 @@
-ansible -v -b -m shell -a 'echo $(hostname),$(lshw -json | jq ".children[0].product"),$(lshw -json | jq ".children[0].children[0].version")' all | grep -E '(cic|dnp)ws[0-9]+,'
+ansible -v -b -m shell -a 'echo $(hostname),$(lshw -json | jq ".children[0].product"),$(lshw -json | jq ".children[0].children[0].version"),$(lshw -json | jq ".children[0].serial")' all | grep -E -o '(cic|dnp)(ws|cs)[0-9]+,.*' | sort

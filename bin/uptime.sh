46bc1c2cef6dbb3a470441034329c407022267c3
Gabriel A. Devenyi <gdevenyi@gmail.com>
2025-09-19 11:16:07 -0400

    Update uptime script

--- bin/uptime.sh
+++ bin/uptime.sh
@@ -2,4 +2,5 @@
 
 
 # Should be replaced with an ansible -m setup call
-ansible -m shell -a 'echo $(hostname),$(uptime)' all -vv |& grep average
+#ansible -m shell -a 'echo $(hostname),$(uptime)' all -vv |& grep average
+ansible -b -m shell -a 'echo $(hostname),$(uptime)' all | grep -E '^(cic|dnp).*,' | sort -V

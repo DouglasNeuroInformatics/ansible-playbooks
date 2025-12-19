74e6859a3025159f37257e30644c0316aed26d39
Gabriel A. Devenyi <gdevenyi@gmail.com>
2025-12-19 11:32:51 -0500

    Remove laborajah3 from tracking

--- bin/find-ips.sh
+++ bin/find-ips.sh
@@ -6,9 +6,9 @@
   done
 done
 nslookup cicvmhost01 | tail -n +3 | grep -E '(Name:|Address:)' | awk -F'\t| +' '{print $2}' | paste -s -d,
-nslookup laborajah3 | tail -n +3 | grep -E '(Name:|Address:)' | awk -F'\t| +' '{print $2}' | paste -s -d,
 nslookup cicbs | tail -n +3 | grep -E '(Name:|Address:)' | awk -F'\t| +' '{print $2}' | paste -s -d,
 nslookup bruker7t | tail -n +3 | grep -E '(Name:|Address:)' | awk -F'\t| +' '{print $2}' | paste -s -d,
+nslookup dnpus01 | tail -n +3 | grep -E '(Name:|Address:)' | awk -F'\t| +' '{print $2}' | paste -s -d,
 ) | sort | uniq
 
 #| grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort | uniq
